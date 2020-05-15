4cf0739ef7b7e68cc427e43edebdac7fb312b88d
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2020-05-15 08:57:48 +0100

    validate_contacts

---
+++ validate_contacts.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv
+import re
+
+import ldap
+import ldap.filter
+
+with open("repos.csv", encoding="utf-8") as csvfile:
+    emails = set()
+    for row in csv.DictReader(csvfile):
+        m = re.search("<(.*)>", row["contact"])
+        if m:
+            emails.add(m.group(1))
+
+ldap_conn = ldap.initialize("ldaps://unixldap.cc.ic.ac.uk")
+for email in emails:
+    if not ldap_conn.search_s(
+        "ou=People,ou=shibboleth,dc=ic,dc=ac,dc=uk",
+        ldap.SCOPE_SUBTREE,
+        "mail=%s" % ldap.filter.escape_filter_chars(email, 1),
+    ):
+        print(email)
